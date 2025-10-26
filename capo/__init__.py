68f00a7a9558b87771424cd3fb0aed122e7dded0
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-26 15:29:07 +0330

    first files added

---
+++ capo/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""capo modules."""
+from capo.params import CAPO_VERSION
+__version__ = CAPO_VERSION
---
+++ capo/params.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+"""capo params."""
+CAPO_VERSION = "0.1"

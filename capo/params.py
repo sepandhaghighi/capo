ef7634e34e97af20917bf2e9a5cf9ad27b4e1d4c
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-10-28 14:25:51 +0330

    feat : params.py updated

--- capo/params.py
+++ capo/params.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 """capo params."""
 CAPO_VERSION = "0.1"
+
+NOTES_SHARP = ["C", "C#", "D", "D#", "E", "F",
+               "F#", "G", "G#", "A", "A#", "B"]
+
+NOTES_FLAT = ["C", "Db", "D", "Eb", "E", "F",
+              "Gb", "G", "Ab", "A", "Bb", "B"]
+
+ENHARMONIC_EQUIVALENTS = {
+    "Db": "C#",
+    "Eb": "D#",
+    "Gb": "F#",
+    "Ab": "G#",
+    "Bb": "A#"
+}
+
+REVERSE_EQUIVALENTS = {v: k for k, v in ENHARMONIC_EQUIVALENTS.items()}

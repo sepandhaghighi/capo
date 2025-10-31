ee43d7e909cd2871ebb0b0190f66226753a50a4e
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-01 01:25:04 +0330

    fix : __init__.py updated

--- capo/__init__.py
+++ capo/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """capo modules."""
 from capo.params import CAPO_VERSION
+from capo.errors import CapoValidationError
 from capo.functions import capo_map
 __version__ = CAPO_VERSION

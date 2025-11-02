b8e81711069a319df6f84de14ba82184a9aa2963
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-03 00:25:55 +0330

    fix : flat_mode=True tests added

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -29,6 +29,13 @@ def test_capo_map_sharp_chords5():  # Reference: https://www.guitarplayerbox.com
     assert result == ['G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G']
 
 
+def test_capo_map_sharp_chords6():
+    # Reference1: https://www.guitarplayerbox.com/chord/list/capo/calculator/
+    # Reference2: https://www.musictheoryacademy.com/understanding-music/enharmonic-equivalents/
+    result = capo_map(chords=['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#'], current_capo=3, target_capo=6, flat_mode=True)
+    assert result == ['Ab', 'A', 'A#', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'Bb']
+
+
 def test_capo_map_flat_chords1():  # Reference: https://bjmorrissey.github.io/capo_calculator/
     result = capo_map(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"], current_capo=0, target_capo=2)
     assert result == ['A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A']
@@ -37,6 +44,13 @@ def test_capo_map_flat_chords1():  # Reference: https://bjmorrissey.github.io/ca
 def test_capo_map_flat_chords2():  # Reference: https://bjmorrissey.github.io/capo_calculator/
     result = capo_map(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"], current_capo=0, target_capo=4)
     assert result == ['G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G']
+
+
+def test_capo_map_flat_chords3():
+    # Reference1: https://bjmorrissey.github.io/capo_calculator/
+    # Reference2: https://www.musictheoryacademy.com/understanding-music/enharmonic-equivalents/
+    result = capo_map(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"], current_capo=0, target_capo=4, flat_mode=True)
+    assert result == ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G']
 
 
 

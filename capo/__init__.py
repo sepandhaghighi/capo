acd72b1b7f8ff61a678969de028dad4c84568af4
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-08 19:44:40 +0330

    fix : __init__.py updated

--- capo/__init__.py
+++ capo/__init__.py
@@ -2,5 +2,5 @@
 """capo modules."""
 from capo.params import CAPO_VERSION
 from capo.errors import CapoValidationError
-from capo.functions import capo_map
+from capo.functions import capo_map, transpose
 __version__ = CAPO_VERSION

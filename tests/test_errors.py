66055dd350be66a3b9734235d461d9cd084445d0
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-09 18:39:00 +0330

    fix : chords_error test cases refactored

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -4,22 +4,22 @@ from capo import capo_map, transpose, CapoValidationError
 TEST_CASE_NAME = "Errors tests"
 
 
-def test_chords_error1():
+def test_capo_map_chords_error1():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = capo_map(chords={"A", "Am"}, current_capo=0, target_capo=2)
 
 
-def test_chords_error2():
+def test_capo_map_chords_error2():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = capo_map(chords=["A", "Am", 1], current_capo=0, target_capo=2)
 
 
-def test_chords_error3():
+def test_transpose_chords_error1():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = transpose(chords={"A", "Am"}, semitones=1)
 
 
-def test_chords_error4():
+def test_transpose_chords_error2():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = transpose(chords=["A", "Am", 1], semitones=1)
 

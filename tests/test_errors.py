ad6d3adf687199c3d1d8a57320ade460cfea1881
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-11-10 19:53:11 +0330

    Refactor (#13)
    * fix : chords_error test cases refactored
    
    * fix : capo_position_error test cases refactored
    
    * fix : error test cases refactored

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -4,56 +4,56 @@ from capo import capo_map, transpose, CapoValidationError
 TEST_CASE_NAME = "Errors tests"
 
 
-def test_chords_error1():
+def test_capo_map_chords_error1():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = capo_map(chords={"A", "Am"}, current_capo=0, target_capo=2)
 
 
-def test_chords_error2():
+def test_capo_map_chords_error2():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = capo_map(chords=["A", "Am", 1], current_capo=0, target_capo=2)
 
 
-def test_chords_error3():
+def test_transpose_chords_error1():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = transpose(chords={"A", "Am"}, semitones=1)
 
 
-def test_chords_error4():
+def test_transpose_chords_error2():
     with pytest.raises(CapoValidationError, match=r"`chords` must be a list of strings."):
         _ = transpose(chords=["A", "Am", 1], semitones=1)
 
 
-def test_capo_position_error1():
+def test_capo_map_capo_position_error1():
     with pytest.raises(CapoValidationError, match=r"capo position must be a non-negative integer."):
         _ = capo_map(chords=["A", "Am", "D"], current_capo=-1, target_capo=2)
 
 
-def test_capo_position_error2():
+def test_capo_map_capo_position_error2():
     with pytest.raises(CapoValidationError, match=r"capo position must be a non-negative integer."):
         _ = capo_map(chords=["A", "Am", "D"], current_capo=0, target_capo=-2)
 
 
-def test_capo_position_error3():
+def test_capo_map_capo_position_error3():
     with pytest.raises(CapoValidationError, match=r"capo position must be a non-negative integer."):
         _ = capo_map(chords=["A", "Am", "D"], current_capo=0, target_capo=1.2)
 
 
-def test_capo_position_error4():
+def test_capo_map_capo_position_error4():
     with pytest.raises(CapoValidationError, match=r"capo position must be a non-negative integer."):
         _ = capo_map(chords=["A", "Am", "D"], current_capo=1.2, target_capo=4)
 
 
-def test_semitones_error():
+def test_transpose_semitones_error():
     with pytest.raises(CapoValidationError, match=r"`semitones` must be an integer."):
         _ = transpose(chords=["A", "Am", "D"], semitones=1.3)
 
 
-def test_chord_format_error1():
+def test_capo_map_chord_format_error():
     with pytest.raises(CapoValidationError, match=r"invalid chord format or unknown note: `s`"):
         _ = capo_map(chords=["A", "Am", "s"], current_capo=0, target_capo=2)
 
 
-def test_chord_format_error2():
+def test_transpose_chord_format_error():
     with pytest.raises(CapoValidationError, match=r"invalid chord format or unknown note: `s`"):
         _ = transpose(chords=["A", "Am", "s"], semitones=2)

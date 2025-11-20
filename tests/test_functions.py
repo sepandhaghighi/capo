a22fdb86753a775fc26598b54537a8760664dd08
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-20 14:58:29 +0330

    fix : tests updated

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -32,7 +32,7 @@ def test_capo_map_sharp_chords6():
     # Reference1: https://www.guitarplayerbox.com/chord/list/capo/calculator/
     # Reference2: https://www.musictheoryacademy.com/understanding-music/enharmonic-equivalents/
     result = capo_map(chords=['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#'], current_capo=3, target_capo=6, flat_mode=True)
-    assert result == ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G']
+    assert result == ['Ab', 'A', 'Bb', 'Cb', 'C', 'Db', 'D', 'Eb', 'Fb', 'F', 'Gb', 'G']
 
 
 def test_capo_map_flat_chords1():  # Reference: https://bjmorrissey.github.io/capo_calculator/
@@ -49,7 +49,7 @@ def test_capo_map_flat_chords3():
     # Reference1: https://bjmorrissey.github.io/capo_calculator/
     # Reference2: https://www.musictheoryacademy.com/understanding-music/enharmonic-equivalents/
     result = capo_map(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"], current_capo=0, target_capo=4, flat_mode=True)
-    assert result == ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G']
+    assert result == ['Ab', 'A', 'Bb', 'Cb', 'C', 'Db', 'D', 'Eb', 'Fb', 'F', 'Gb', 'G']
 
 
 def test_capo_map_slash_chords1():  # Reference: https://muted.io/chord-transposer/
@@ -88,20 +88,20 @@ def test_transpose_sharp_chords3():  # Reference: https://muted.io/chord-transpo
 
 
 def test_transpose_flat_chords1():  # Reference: https://muted.io/chord-transposer/
-    result = transpose(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"], semitones=-2)
-    assert result == ['A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A']
+    result = transpose(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "Cb", "Fb"], semitones=-2)
+    assert result == ['A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A', 'D']
 
 
 def test_transpose_flat_chords2():  # Reference: https://muted.io/chord-transposer/
-    result = transpose(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"], semitones=-4)
-    assert result == ['G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G']
+    result = transpose(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "Cb", "Fb"], semitones=-4)
+    assert result == ['G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G', 'C']
 
 
 def test_transpose_flat_chords3():
     # Reference1: https://muted.io/chord-transposer/
     # Reference2: https://www.musictheoryacademy.com/understanding-music/enharmonic-equivalents/
-    result = transpose(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"], semitones=-4, flat_mode=True)
-    assert result == ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G']
+    result = transpose(chords=["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "Cb", "Fb"], semitones=-4, flat_mode=True)
+    assert result == ['Ab', 'A', 'Bb', 'Cb', 'C', 'Db', 'D', 'Eb', 'Fb', 'F', 'Gb', 'G', 'G', 'C']
 
 
 def test_transpose_slash_chords1():  # Reference: https://muted.io/chord-transposer/

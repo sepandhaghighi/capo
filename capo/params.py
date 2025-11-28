35724b97427caef9adff2b5b70c24c8079ec931c
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-11-28 14:49:14 +0330

    feat : KRUMHANSL_SCHMUCKLER profiles added

--- capo/params.py
+++ capo/params.py
@@ -34,6 +34,12 @@ ENHARMONIC_EQUIVALENTS = {
     "Cbb": "A#",
 }
 
+KRUMHANSL_SCHMUCKLER_MAJOR = [6.35, 2.23, 3.48, 2.33, 4.38, 4.09,
+            2.52, 5.19, 2.39, 3.66, 2.29, 2.88]
+
+KRUMHANSL_SCHMUCKLER_MINOR = [6.33, 2.68, 3.52, 5.38, 2.60, 3.53,
+            2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
+
 CHORDS_TYPE_ERROR_MESSAGE = "`chords` must be a list of strings."
 CAPO_POSITION_ERROR_MESSAGE = "capo position must be a non-negative integer."
 SEMITONES_TYPE_ERROR_MESSAGE = "`semitones` must be an integer."

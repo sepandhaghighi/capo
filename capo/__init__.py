87159557bc77959ad4c71ae97b9fda858f46a237
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-02-08 15:49:03 +0330

    fix : __init__.py updated

--- capo/__init__.py
+++ capo/__init__.py
@@ -4,6 +4,7 @@ from capo.params import CAPO_VERSION
 from capo.errors import CapoValidationError
 from capo.functions import capo_map, transpose, transpose_to_key
 from capo.functions import detect_key, key_scores
+from capo.functions import sharp_to_flat
 __version__ = CAPO_VERSION
 
-__all__ = ["CapoValidationError", "capo_map", "transpose", "transpose_to_key", "detect_key", "key_scores"]
+__all__ = ["CapoValidationError", "capo_map", "transpose", "transpose_to_key", "detect_key", "key_scores", "sharp_to_flat"]

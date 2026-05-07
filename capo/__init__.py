5707a797fe433079a24f3c62408868ff79bacb7f
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-07 15:14:42 +0330

    fix : add CapoError to __init__.py file

--- capo/__init__.py
+++ capo/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """capo modules."""
 from .params import CAPO_VERSION
-from .errors import CapoValidationError
+from .errors import CapoError, CapoValidationError
 from .functions import capo_map, transpose, transpose_to_key
 from .functions import detect_key, key_scores
 from .functions import sharp_to_flat, flat_to_sharp
 __version__ = CAPO_VERSION
 
 __all__ = [
+    "CapoError",
     "CapoValidationError",
     "capo_map",
     "transpose",
